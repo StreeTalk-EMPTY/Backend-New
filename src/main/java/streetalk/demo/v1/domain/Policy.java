a87c7cc7b77813f2f59607e9c6cc8b8280d6c53b
pxxnxx <kmin9811@naver.com>
2023-07-07 14:54:55 +0900

    이용약관 추가 및 Workflow Dispatch

---
+++ src/main/java/streetalk/demo/v1/domain/Policy.java
@@ -0,0 +1,23 @@
+package streetalk.demo.v1.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Policy {
+
+    @Id
+    private Long id;
+    @Column(length = 1000)
+    private String termsOfUse;
+    @Column(length = 1000)
+    private String privatePolicy;
+}
