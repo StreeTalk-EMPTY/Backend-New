e388a7684962243af5e6d265a261962724cce961
pxxnxx <kmin9811@naver.com>
2023-07-19 12:11:58 +0900

    Scrap 오타 교체

--- src/main/java/streetalk/demo/v1/repository/PostScrapRepository.java
+++ src/main/java/streetalk/demo/v1/repository/PostScrapRepository.java
@@ -3,16 +3,16 @@ package streetalk.demo.v1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import streetalk.demo.v1.domain.Post;
-import streetalk.demo.v1.domain.PostScarp;
+import streetalk.demo.v1.domain.PostScrap;
 import streetalk.demo.v1.domain.User;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PostScrapRepository extends JpaRepository<PostScarp,Long> {
-    Optional<PostScarp>findByPostAndUser(Post post, User user);
-    List<PostScarp>findByUser(User user);
+public interface PostScrapRepository extends JpaRepository<PostScrap,Long> {
+    Optional<PostScrap>findByPostAndUser(Post post, User user);
+    List<PostScrap>findByUser(User user);
 
 //    void delete(Optional<PostScarp> scrapLike);
 }
