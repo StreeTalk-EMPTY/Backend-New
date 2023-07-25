01f74fb5197e92ee889dcfb27d350cbe08e5065d
pxxnxx <kmin9811@naver.com>
2023-07-25 09:39:01 +0900

    PUT Post 테스트

--- src/main/java/streetalk/demo/v1/repository/PostImagesRepository.java
+++ src/main/java/streetalk/demo/v1/repository/PostImagesRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import streetalk.demo.v1.domain.PostImages;
 
+@Repository
 public interface PostImagesRepository extends JpaRepository<PostImages, Long> {
 
     void deleteAllByPostId(Long postId);
