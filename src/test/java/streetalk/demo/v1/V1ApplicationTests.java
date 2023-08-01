3e5f2dca62415c396e4b87a16cea3ab17d72be4c
김민석 <kmin9811@naver.com>
2023-08-01 11:55:04 +0900

    searchPost content 검색 + 차단 유저 생성(현재는 업종 게시판만) #2

--- src/test/java/streetalk/demo/v1/V1ApplicationTests.java
+++ src/test/java/streetalk/demo/v1/V1ApplicationTests.java
@@ -2,12 +2,14 @@ package streetalk.demo.v1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import streetalk.demo.v1.domain.User;
 
 @SpringBootTest
 class V1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
